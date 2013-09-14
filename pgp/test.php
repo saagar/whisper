0dce24e8bf15bcc0d66cd7248388adc2f747fbba
Saagar Deshpande <sdeshpande@college.harvard.edu>
2013-09-14 04:05:46 -0400

    pushing the encryption stuff. encrypt and dec work, need to finish signing

---
+++ pgp/test.php
@@ -0,0 +1,17 @@
+
+<!doctype html>
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Example | OpenPGP.js</title>
+
+ </head>
+ <body>
+  <div id="info">
+
+  </div>
+  <script src="openpgp/resources/jquery.min.js" type="text/javascript" charset="utf-8"></script>
+  <script src="openpgp/resources/openpgp.js" type="text/javascript" charset="utf-8"></script>
+  <script src="./example.js" type="text/javascript" charset="utf-8"></script>
+ </body>
+</html>
