d0e99f246f4c55120649c91548d840ead50daca1
Martin Camacho <camacho@college.harvard.edu>
2013-09-14 12:57:41 -0700

    pgp encryption somewhat works

--- extension/js/main.js
+++ extension/js/main.js
@@ -3,7 +3,123 @@ var clickedEl = null;
 var _groups = null;
 var user_logged_in = null;
 
+
+//------------- PGP code ----------------------
+// Initialize PGP once
 openpgp.init();
+// var keypair2 = generateKeyPair(1, 1024, "Amy Tai", "noob@pton.edu", "");
+
+// utility functions to get armored keys
+function getArmoredPublicKey(keypair) {
+  return keypair.publicKeyArmored;
+}
+
+function getArmoredPrivateKey(keypair) {
+  return keypair.privateKeyArmored;
+}
+
+// pass in 1 for algo to use RSA because nothing else is supported....
+// leave password as blank for now.
+function generateKeyPair(algo, size, name, email, password) {
+  if (window.crypto.getRandomValues) {
+    var name_email_pair = name + " <" + email + ">";
+    var keypair = openpgp.generate_key_pair(algo, size, name_email_pair, password);
+    chrome.storage.local.set({
+      publicKeyArmored: getArmoredPublicKey(keypair),
+      privateKeyArmored: getArmoredPrivateKey(keypair)
+    });
+    return keypair;
+  }
+  else {
+    return failAndWarn();
+  }
+}
+
+
+var keypair2 = {};
+
+chrome.storage.local.get(['publicKeyArmored', 'privateKeyArmored'], function(data) {
+  if(data.privateKeyArmored && data.publicKeyArmored) {
+    keypair2 = data;
+  } else {
+    keypair2 = generateKeyPair(1, 1024, "Amy Tai", "noob@pton.edu", "");
+  }
+  console.log(keypair2.privateKeyArmored);
+});
+
+
+// need a private armored key - password should be blank for now
+function pgpDecrypt(privateKey, ciphertext, password) {
+  if (window.crypto.getRandomValues) {
+    var priv_key = openpgp.read_privateKey(privateKey);
+
+    if (priv_key.length < 1) {
+      util.print_error("No private key found!")
+      return;
+    }
+
+    // here's where the decrypting magic happens. don't ask, just do it.
+    var msg = openpgp.read_message(ciphertext);
+    if(!msg) return null;
+    var keymat = null;
+    var sesskey = null;
+    // Find the private (sub)key for the session key of the message
+    for (var i = 0; i< msg[0].sessionKeys.length; i++) {
+      if (priv_key[0].privateKeyPacket.publicKey.getKeyId() == msg[0].sessionKeys[i].keyId.bytes) {
+        keymat = { key: priv_key[0], keymaterial: priv_key[0].privateKeyPacket};
+        sesskey = msg[0].sessionKeys[i];
+        break;
+      }
+      for (var j = 0; j < priv_key[0].subKeys.length; j++) {
+        if (priv_key[0].subKeys[j].publicKey.getKeyId() == msg[0].sessionKeys[i].keyId.bytes) {
+          keymat = { key: priv_key[0], keymaterial: priv_key[0].subKeys[j]};
+          sesskey = msg[0].sessionKeys[i];
+          break;
+        }
+      }
+    }
+
+    // decrypt the string
+    if (keymat != null) {
+      if (!keymat.keymaterial.decryptSecretMPIs(password)) {
+        util.print_error("Password for secrect key was incorrect!");
+        return;
+      }
+      plaintext = msg[0].decrypt(keymat, sesskey);
+      return plaintext;
+    } else {
+      util.print_error("No private key found!");
+    }
+
+  } else {
+    return failAndWarn();
+  }
+}
+
+function failAndWarn() {
+  window.alert("Error: Browser not supported\nReason: We need a cryptographically secure PRNG to be implemented (i.e. the window.crypto method)\nSolution: Use Chrome >= 11, Safari >= 3.1 or Firefox >= 21");
+  return false;
+}
+
+// need a public armored key
+function pgpEncrypt(publicKey, plaintext) {
+  if (window.crypto.getRandomValues) {
+
+    var publicKeyObject = openpgp.read_publicKey(publicKey);
+
+    return openpgp.write_encrypted_message(publicKeyObject, plaintext);
+  // // var publicKey = openpgp.read_publicKey(keypair2.publicKeyArmored);
+  // // ciphertext = openpgp.write_encrypted_message(publicKey, "Hello World!");
+  //   var publicKeyObject = openpgp.read_publicKey(publicKey);
+  //   ciphertext = openpgp.write_encrypted_message(publicKeyObject, plaintext);
+  //   return ciphertext;
+  }
+  return failAndWarn();
+}
+
+
+//------------- Extension code --------------------
+
 
 document.addEventListener("mousedown", function(event){
     //right click
@@ -37,7 +153,7 @@ function rot13(s)
      }).join('');
  }
 
-var wispRegex = /\[\!wisp \| (\w*) \| (\w*) \] ([^\[\|]*) \| ([^\[\|]*) \[\/\/wisp\]/gi;
+var wispRegex = /\[\!wisp \| (\w*) \| (\w*) \]([^\[\|]*)\|([^\[\|]*)\[\/\/wisp\]/gi;
 
 function wispRegexReplacer(match, sender, recipient, senderMessage, recipientMessage, offset, string) {
   // console.log(sender, recipient, user_logged_in, senderMessage, recipientMessage);
@@ -51,12 +167,25 @@ function wispRegexReplacer(match, sender, recipient, senderMessage, recipientMes
 
 // value is text
 function encrypt(key, group_id, value){
-  //encrypted = CryptoJS.AES.encrypt(value, key);
-  var aes = new pidCrypt.AES.CBC();
-  var encrypted = aes.encryptText(value, key, {nBits: 256});
+  console.log(keypair2);
+
+  var publicKey = openpgp.read_publicKey(getArmoredPublicKey(keypair2));
+
+  // // var text = "It is a rare to watch someone secure a freshly installed server right off the bat, yet the world we live in makes this a necessity. So why do so many people put it off until the end, if at all? I’ve done the exact same thing, and it often comes down to wanting to get right into the fun stuff. Hopefully this post will show that it is far easier than you think to secure a server, and can be quite entertaining to look down from your fortress, when the attacks begin to flow."
+
+  var ciphertext = btoa(pgpEncrypt(getArmoredPublicKey(keypair2), value));
+
+  console.log(ciphertext);
+  console.log(btoa(ciphertext));
+
+  var plaintext = pgpDecrypt(getArmoredPrivateKey(keypair2), atob(ciphertext), "");
+  console.log(plaintext);
+
+
+  // var encrypted = aes.encryptText(value, key, {nBits: 256});
   var encrypted = rot13(value); // this is temporary, rewrite this later
-  return "[!wisp | " + user_logged_in + " | " +  group_id + " ] " + encrypted + " | "
-    + encrypted + " [//wisp]";
+  return "[!wisp | " + user_logged_in + " | " +  group_id + "]" + ciphertext + "|"
+    + encrypted + "[//wisp]";
 }
 
 // Scan element for wisp tags and decrypt if possible.
@@ -66,11 +195,17 @@ function decrypt (element) {
   if (html.length > 0) {
     for (var i = html.length-1; i >= 0; i--) {
       var ele = $(html[i]);
+      if(ele.is('span.userContent')) console.log(ele.html());
       // Only replace text in elements with no children. Works as long as text
       // is not weirdly formatted.
-      if(ele.children().length > 0) continue;
+      // .userContent is FB div
+      if(ele.children().not('wbr').length > 0 || ele.is('span.userContent')) continue;
+
+      ele.find('wbr').remove();
+      console.log(ele);
 
       var text = ele.html();
+      console.log(text);
       if (text.indexOf("[!wisp | ") != -1 && text.indexOf("[//wisp]") != -1)
       {
         ele.text(text.replace(wispRegex, wispRegexReplacer));
@@ -78,6 +213,8 @@ function decrypt (element) {
     }
   }
 }
+
+function showMessages(msg){console.log(msg);};
 
 $.getJSON('http://whisper-signalfire.herokuapp.com/keystore/user').done(function(data){
   user_logged_in = data.user;
@@ -89,9 +226,21 @@ $.getJSON('http://whisper-signalfire.herokuapp.com/keystore/user').done(function
 // if so, attempts to use the user's private key to decrypt.
 function decrypt_msg(sender, recipient, senderMessage, recipientMessage)
 {
+  console.log(senderMessage);
   if (sender === user_logged_in){
+    var unb64 = null;
+    try {
+      unb64 = atob(senderMessage);
+    } catch(err) {
+      return "[whisper] Undecryptable message. [//whisper]"; 
+    }
     // unecrypt using user's private key later
-    return "(wisp to " + recipient + "): " + rot13(senderMessage);
+    var decrypted = pgpDecrypt(getArmoredPrivateKey(keypair2), unb64, "");
+    if (!decrypted) {
+      return "[whisper] Undecryptable message. [//whisper]";
+    }
+    return "(wisp to " + recipient + "): " +
+      decrypted;
   } else if (recipient === user_logged_in) {
     return "(wisp from " + sender + "): " + rot13(recipientMessage);
   }
