8c538d4503db52fa29c80c6909ced950fa97555e
bksim <simpavid@gmail.com>
2013-09-13 21:08:49 -0700

    rot13 and other stuff

--- extension/js/main.js
+++ extension/js/main.js
@@ -23,50 +23,69 @@ chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 		EncryptedForms.syncGroups();
 	}
 });
-    
+
+// implements a basic rot13
+function rot13(s)
+ {
+    return (s ? s : this).split('').map(function(_)
+     {
+        if (!_.match(/[A-za-z]/)) return _;
+        c = Math.floor(_.charCodeAt(0) / 97);
+        k = (_.toLowerCase().charCodeAt(0) - 83) % 26 || 26;
+        return String.fromCharCode(k + ((c == 0) ? 64 : 96));
+     }).join('');
+ }
+
+// AES encryption
+// key is private key
+// value is text
 function encrypt(key, group_id, value){
 	//encrypted = CryptoJS.AES.encrypt(value, key);
 	var aes = new pidCrypt.AES.CBC();
 	var encrypted = aes.encryptText(value, key, {nBits: 256});
+	var encrypted = rot13(value); // this is temporary, rewrite this later
 	return "[!wisp | " + group_id + " ] " + encrypted + " [/wisp]";
 }
 
-    function decrypt (element)
-	{
-	  var html = $(element).find('*:contains("[!wisp | ")');
-	  if (html.length > 0)
-	  {
-		 for (var i = html.length-1; i >= 0; i--)
-		 {
-			var ele = $(html[i]);
-			var text = ele.text();
-			if (text.indexOf("[!wisp | ") != -1 && text.indexOf("[/wisp]") != -1)
-			{
-				var result = text.split("[")[1].split("]");
-				var number = result[0].split("|")[1].trim();
-				var encrypted = result[1].trim();
-				ele.text(decrypt_msg(number, encrypted));
-				decrypt();
-			}
-		 }
-	  }
-	}
-	
-
-	function decrypt_msg(group_id, msg)
-	{
-		var key = EncryptedForms.groups[group_id];
-		if (key == null ){
-			return "You don't have access to this message.";
-		}
-		else
+// AES decrypt, after parsing the tag. calls decrypt_msg
+function decrypt (element)
+{
+  var html = $(element).find('*:contains("[!wisp | ")');
+  if (html.length > 0)
+  {
+	 for (var i = html.length-1; i >= 0; i--)
+	 {
+		var ele = $(html[i]);
+		var text = ele.text();
+		if (text.indexOf("[!wisp | ") != -1 && text.indexOf("[/wisp]") != -1)
 		{
-			var aes = new pidCrypt.AES.CBC();
-			var decrypted = aes.decryptText(msg, key, {nBits: 256});
-			return decrypted;
+			var result = text.split("[")[1].split("]");
+			var number = result[0].split("|")[1].trim();
+			var encrypted = result[1].trim();
+			ele.text(decrypt_msg(number, encrypted));
+			decrypt();
 		}
+	 }
+  }
+}
+	
+// AES decryption using group id to get the private key
+function decrypt_msg(group_id, msg)
+{
+	var key = EncryptedForms.groups[group_id];
+	if (key == null ){
+		return msg // TEMPORARY
+		//return "You don't have access to this message.";
 	}
+	else
+	{
+		var aes = new pidCrypt.AES.CBC();
+		var decrypted = aes.decryptText(msg, key, {nBits: 256});
+		return decrypted;
+	}
+}
 
+// gets locally stored username information
 chrome.storage.local.get(["username", "pw"], function(data)
 {
 	$.getJSON("http://www.projectvoid.com/whisper/whisper_controller.php?action=retrieve&username="+data['username']+"&pw="+data['pw'], function (data)
